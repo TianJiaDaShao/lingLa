c428b40059ddb825963745485435dc44d1ac2511
TianYe <1924317972@qq.com>
2016-10-08 17:51:06 +0800

    invite

---
+++ src/scripts/views/inv.js
@@ -0,0 +1,6 @@
+var str = require('../tpls/inv.string');
+
+var common = require('../utils/common.util.js');
+var iscrollUtil = require('../utils/iscroll.util.js');
+
+common.renderBody($('body'), str);
